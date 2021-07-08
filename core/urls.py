9e05348028fe72b9d15f1e19bcb500f78c5a2d82
jorgeduoc <81388508+jorgeduoc@users.noreply.github.com>
2021-07-07 21:32:42 -0400

    agregar path y views
    agregue path y views de las url

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import Agregar_Obra, Artistas, home
+from .views import Agregar_Obra, Artistas, Conocenos, home, Login, Nuevas_Obras, registro, Revisar_Obras
 
 urlpatterns = [
     path('',home,name="home"),
     path('Agregar_Obras/',Agregar_Obra,name="Agregar_Obra"),
-    path('Artistas/',Artistas,name="Artistas")
+    path('Artistas/',Artistas,name="Artistas"),
+    path('Conocenos/',Conocenos,name="Conocenos"),
+    path('Login/',Login,name="Login"),
+    path('Nuevas_Obras/',Nuevas_Obras,name="Nuevas_Obras"),
+    path('registro/',registro,name="registro"),
+    path('Revisar_Obras/',Revisar_Obras,name="Revisar_Obras"),
 ]
