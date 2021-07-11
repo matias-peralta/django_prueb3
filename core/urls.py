f36ddd51b09dba9812f91e21429edc5597eb1179
jorgeduoc <81388508+jorgeduoc@users.noreply.github.com>
2021-07-11 02:21:38 -0400

    login

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import Agregar_Obra, Artistas, Conocenos, home, Login, Nuevas_Obras, registro, Revisar_Obras
-
+from django.contrib.auth import login
 urlpatterns = [
     path('',home,name="home"),
     path('Agregar_Obras/',Agregar_Obra,name="Agregar_Obra"),
@@ -10,4 +10,7 @@ urlpatterns = [
     path('Nuevas_Obras/',Nuevas_Obras,name="Nuevas_Obras"),
     path('registro/',registro,name="registro"),
     path('Revisar_Obras/',Revisar_Obras,name="Revisar_Obras"),
+    path(r'^accounts/login/$',  login),
+
+ 
 ]
