b1a09fc9b9e88d5e76106e647bb8c50e334c48b5
matias-peralta <ma.peraltac@duocuc.cl>
2021-07-11 21:58:49 -0400

    creacion de los modelos y las migraciones

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-
+from .models import Artista,obras,Usuario
 # Register your models here.
-
+admin.site.register(Artista)
+admin.site.register(obras)
+admin.site.register(Usuario)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+class Artista(models.Model):
+    nombre_Artista = models.CharField(max_length=50, verbose_name="nombre del artista")
+    edad_Artista = models.IntegerField(verbose_name="Edad")
+    nacimiento_Artista = models.CharField(max_length=50, verbose_name="Nacimiento")
+    tecnica_Artista = models.CharField(max_length=40, verbose_name="Tecnica")
+    descripcion_Artista= models.CharField(max_length=200, verbose_name="Descripcion")
+    id_Artista = models.AutoField(primary_key=True,verbose_name="id Artista")
+
+    def __str__(self) :
+        return self.nombre_Artista
+
+class obras(models.Model):
+    id_obra =models.AutoField(primary_key=True,verbose_name="id de la obra")
+    titulo_obra=models.CharField(max_length=30,verbose_name="titulo de la obra")
+    precio_obra=models.IntegerField(verbose_name="precio de la obra")
+    tecnica_obra=models.CharField(max_length=30,verbose_name="tecnica de la obra")
+    dimenciones_obra=models.IntegerField(verbose_name="dimenciones de la obra")
+    peso_obra=models.IntegerField(verbose_name="peso de la obra")
+    descripcion_obra=models.CharField(max_length=100,verbose_name="descripcion de la obra")
+    imagen_obra=models.CharField(max_length=200,verbose_name="link de la obra")
+    Artista=models.ForeignKey(Artista,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo_obra
+
+
+class Usuario(models.Model):
+    nombre_usuario= models.CharField(max_length=15, verbose_name="nombre de Usuario")
+    clave_usuario= models.CharField(max_length=10, verbose_name="Contraseña")
+    id_usuario = models.AutoField(primary_key=True,verbose_name="id Usuario")
+    telefono_usuario= models.CharField(max_length=10, verbose_name="Telefono Usuario")
+
+    def __str__(self) :
+        return self.nombre_usuario      
