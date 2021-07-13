0c992e2f0c9f49647e0b694c96474e476161bae2
jorgeduoc <81388508+jorgeduoc@users.noreply.github.com>
2021-07-13 19:30:06 -0400

    el login no funciona
    esta comentado

--- testDjango/settings.py
+++ testDjango/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    #'users', se intento crear la aplicacion users
+    
     
 ]
 
--- testDjango/urls.py
+++ testDjango/urls.py
@@ -14,15 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth import login
 from django.urls import path,include
+from django.conf.urls import include, url
 
 
-app_name= 'main'
+
 
 urlpatterns = [
     path('admin/',admin.site.urls),
     path('',include('core.urls')) ,
-
+    #url(r"^", include("users.urls")),
+    #url(r"^admin/", admin.site.urls),
    
 ]
